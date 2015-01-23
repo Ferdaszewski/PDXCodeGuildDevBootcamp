850ca78f5d7e82e0f24f57bcc8715e130c397944
Joshua Ferdaszewski <ferdaszewski@gmail.com>
2015-01-22 23:51:41 -0800

    Skeleton of classes finished

--- 02-doto/doto.py
+++ 02-doto/doto.py
@@ -1,14 +1,71 @@
 """Module with classes for List DoTo application.
 """
+import datetime
 
 
 class Collection(object):
-    pass
+    def __init__(self, tasks):
+        self.tasks = tasks
+
+    def display(self, mode, sort='due_date'):
+        # TODO: Terminal mode + HTML mode for display
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    def find(self):
+        pass
+
+    def sort(self, attribute):
+        pass
 
 
 class Task(object):
-    pass
+    def __init__(self, user, due_date=None):
+        self.entry = ""
+        self.tag = [user,]
+        self.entry_time = datetime.utcnow()
+        self.creator = user
+        self.due_date = due_date
+        self.done = [False,]
+        self.id = 0
+
 
 
 class Storage(object):
-    pass
+    def __init__(self):
+        self.load()
+        pass
+
+    def __getitem__(self, index):
+        # Return a collection object that is loaded from file
+        temp = Collection('tasks')
+        return temp
+        pass
+
+    def save(self):
+        # TODO: save collections to file
+        pass
+
+    def load(self):
+        # TODO: load collections from file
+        pass
+
+    def read(self):
+        # static or class methods?
+        pass
+
+    def write(self):
+        # static or class methods?
+        pass
+
+    def change_coll(self, collection):
+        # change collection
+        pass
--- 02-doto/dtmain.py
+++ 02-doto/dtmain.py
@@ -1,10 +1,15 @@
 """Do To application.
 Main control structure.
 """
+import sys
+
 import doto
 
 
 class DoToApp(object):
+    def __init__(self):
+        self.master_collection = doto.Storage()
+        self.cur_collection = self.master_collection[0]
     
     def load(self):
         """Initial loading screen, user selects where to load/save data.
@@ -13,14 +18,50 @@ class DoToApp(object):
         print "Welcome to Do To! What is your name?"
         name = raw_input("> ")
 
-        # if this is a new user, prompt create a new user?
+        # TODO: if this is a new user, prompt create a new user?
+        # TODO: User class needed?
+
         self.main(name)
 
 
 
     def main(self, user):
-        # display first collection in the master_collection
-        # control/navigation at bottom
+        """Command control loop for the application. Displays the first
+        collection in master_collection and loops until user quits.
+        """
+        command = ""
+        while command not in ('q', "quit"):
+            self.clear_screen(user)
+            self.cur_collection.display('term')
+            command = raw_input(
+                "\nEnter command (? for help) > ").strip().lower()
+
+            if command in ('s', 'save'):
+                self.master_collection.save()
+            elif command in ('l', 'load'):
+                self.master_collection.load()
+                self.cur_collection = self.master_collection[0]
+            elif command in ('n', 'new'):
+                self.cur_collection.add()
+            elif command in ('e', 'select'):
+                selcted_task = self.cur_collection.find()
+                # TODO: prompt user to delete or change selected task
+            elif command in ('c', 'change'):
+                # TODO: Get index of collection that user wants to switch to
+                coll_index = 0
+                self.cur_collection = self.master_collection.change_coll(coll_index)
+            elif command in ('o', 'order'):
+                # TODO: prompt user for attribute to sort by
+                # TODO: figure out best way to handle default sort
+                self.cur_collection.sort
+            else:
+                self.clear_screen(user)
+                raw_input("Invalid command. Press Enter to try again ")
+
+
+                
+
+
 
     def clear_screen(self, user=""):
         """Clear screen, return cursor to top left, and display banner.
