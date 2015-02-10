ca556017799c0b5a93c9931243e0401e11d78bd9
Joshua Ferdaszewski <ferdaszewski@gmail.com>
2015-02-10 12:05:49 -0800

    Initial Commit

---
+++ zz-sundry/js-html-css/calc/js/calc.js
@@ -0,0 +1,42 @@
+function calculate(evt) {
+  button = evt.target;
+  var resultDisplay = document.getElementById('result');
+  resultDisplay.className = "result hidden";
+
+  // P tag may have been clicked
+  if (button.tagName === 'BUTTON') {
+    var frm = document.getElementById('calcform');
+    var a = parseFloat(frm.input_a.value, 10);
+    var b = parseFloat(frm.input_b.value, 10);
+  }
+  else {
+    return;
+  }
+
+  // Numbers only please
+  if (isNaN(a) || isNaN(b) || !isFinite(a) || !isFinite(b)) {
+    result = "Numbers Only Please";
+    resultDisplay.className += ' error';
+  }
+  else {
+    var operator = button.name;
+    switch (operator) {
+      case "addbutton":
+        result = a + b;
+        break;
+      case "multbutton":
+        result = a * b;
+        break;
+      case "divbutton":
+        result = a / b;
+        break;        
+    }      
+  }
+  resultDisplay.classList.remove("hidden");
+  resultDisplay.textContent = result;
+}
+
+// Wait until DOM is loaded
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("button_wrapper").onclick = calculate;
+});
